859fdac894ba27bb5b3b34522942330676fb29e0
boya <qj92724275@gmail.com>
2024-04-22 23:13:20 +0800

    加入專案檔案。

---
+++ WebApplication1/Models/DBmanager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DBmanager
+    {
+        private readonly string ConnString = "Data Source=.;Initial Catalog=Testfor;Integrated Security=True;";
+
+        public List<NBandAccoData> GetUserData()
+        {
+            List<NBandAccoData> list = new List<NBandAccoData>();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            string query = "SELECT NBank.NationalID, Account.BranchID, Account.AcctSerialID " +
+                           "FROM NetBankUsers NBank " +
+                           "JOIN Accounts Account " +
+                           "ON NBank.UserGuid = Account.OwnerGuid " +
+                           "WHERE NBank.NationalID = @NationalID";
+
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@NationalID", "K18888886");
+            sqlConnection.Open();
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                NBandAccoData data = new NBandAccoData
+                {
+                    NationalID = reader["NationalID"].ToString(),
+                    BranchID = reader["BranchID"].ToString(),
+                    AcctSerialID = reader["AcctSerialID"].ToString(),
+                };
+
+                list.Add(data);
+            }
+            sqlConnection.Close();
+            return list;
+        }
+    }
+}
---
+++ WebApplication1/Models/NetBankUsers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class NBandAccoData
+    {
+        public string NationalID {  get; set; }
+        public string BranchID { get; set; }
+        public string AcctSerialID { get; set; }
+    }
+}
